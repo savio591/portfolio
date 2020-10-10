a354f0070908d58924e619dc015b51432fc8887f
savio591 <savio591@hotmail.com>
2020-10-09 23:49:26 -0300

    portfolio v2.0

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5c0d929a5d209f6a194c36bd9153cc6a.js"
+  "/portfolio/precache-manifest.eb0054778756547c3693bf745fd606e0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
--- src/index.js
+++ src/index.js
@@ -1,19 +1,63 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './css/index.css';
+import './css/main.css';
+import './css/normalize.css';
 import './css/react-index.css';
+import './main.js';
+import './plugins.js';
+import grid_svg from './grids/grid.graphicdesign.svg'
 import * as serviceWorker from './serviceWorker';
 
-class teste extends React.Component {
+class HeaderPrincipal extends React.Component {
+
+  nome = "SAVIL EXPERIMENTS"
+  categoria = "Portfolio";
+  line = " "
+  header_nome = <h1 className="header_logo">{this.nome}</h1>;
+  header_desc = <h1 className="header_desc">{this.categoria}</h1>;
+  grid_wireframe_svg = <picture></picture>
+
+    render() {
+      return (
+        <div>
+          {this.header_nome}
+          {this.header_desc_line}
+          {this.header_desc}
+        </div>
+        
+      )}
+  }
+
+class ContainerCategorias extends React.Component {
+  grid = <div className="grid">
+    <img
+    srcSet={grid_svg}
+    alt="gridwireframe"
+    className="grid-svg"
+    src={grid_svg}
+    />
+  </div>
   render() {
-    return <h1>Hello, {this.props.name}</h1>;
+    return(
+      <div className="container_categorias">
+        {this.grid}
+        {this.grid}
+        {this.grid}
+      </div>
+    )
   }
 }
+//======================================================================
 
 ReactDOM.render(
-  <React.StrictMode>
-    <teste name="ao"/>
-  </React.StrictMode>,
-  document.getElementById('teste')
+  <HeaderPrincipal />,
+  document.getElementById('HeaderPrincipal')
+);
+
+ReactDOM.render(
+  <ContainerCategorias />,
+  document.getElementById('ContainerCategorias')
 );
 
 // If you want your app to work offline and load faster, you can change
